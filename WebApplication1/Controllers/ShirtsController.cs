4773bd4fa161e09519fe12222a8af2a5ac9dddfa
Panizsh68 <panizshaabanpour16@gmail.com>
2025-09-14 21:17:47 +0330

    Web API Return Types

--- WebApplication1/Controllers/ShirtsController.cs
+++ WebApplication1/Controllers/ShirtsController.cs
@@ -7,39 +7,50 @@ namespace WebApplication1.Controllers
   [Route("api/[controller]")]
   public class ShirtsController : ControllerBase
   {
-    [HttpGet]
-    public string GetShirts()
+    private List<Shirt> shirts = new List<Shirt>()
     {
-      return "Reading all the shirts";
+      new Shirt {ShirtId = 1, Brand = "My Brand", Color = "Blue", Gender = "Male", Price = 30, Size = 10},
+      new Shirt {ShirtId = 2, Brand = "My Brand", Color = "Black", Gender = "Male", Price = 35, Size = 12},
+      new Shirt {ShirtId = 3, Brand = "Your Brand", Color = "Pink", Gender = "Female", Price = 28, Size = 8},
+      new Shirt {ShirtId = 4, Brand = "Your Brand", Color = "Yellow",  Gender = "Female", Price = 30, Size = 9},
+    };
+
+    [HttpGet]
+    public IActionResult GetShirts()
+    {
+      return Ok("Reading all the shirts");
     }
 
     [HttpGet("{id}")]
-    // for FromRoute: 
-    // [HttpGet("{id}/{color}")]
-    public string GetShirtById(int id, [FromHeader(Name = "Color")] string color) // so in header, key is "Color"
-    // public string GetShirtById(int id, [FromQuery] string color) so in query, you will do this: ?color=red in the url
-    // public string GetShirtById(int id, [FromRoute] string color) this will ensure color is taken from the route
+    // public Shirt GetShirtById(int id) : this means you want to return only Shirt
+    public IActionResult GetShirtById(int id) // here means you want to return different types of responses
     {
-      return $"Reading shirt with id: {id} and color: {color}";
+      // return shirts.First(x => x.ShirtId == id); : if not found, it will throw an unfriendly exception
+      if (id <= 0)
+        return BadRequest();
+      var shirt = shirts.FirstOrDefault(x => x.ShirtId == id);
+      if (shirt == null)
+        return NotFound();
+      return Ok(shirt);
     }
 
     [HttpPost]
-    public string CreateShirt([FromBody]Shirt shirt)
+    public IActionResult CreateShirt([FromBody]Shirt shirt)
     // public string CreateShirt([FromForm]Shirt shirt)
     {
-      return $"Creating a new shirt";
+      return Ok($"Creating a new shirt");
     }
 
     [HttpPut("{id}")]
-    public string UpdateShirt(int id)
+    public IActionResult UpdateShirt(int id)
     {
-      return $"Updating shirt with id: {id}";
+      return Ok($"Updating shirt with id: {id}");
     }
 
     [HttpDelete("{id}")]
-    public string DeleteShirt(int id)
+    public IActionResult DeleteShirt(int id)
     {
-      return $"Deleting shirt with id: {id}";
+      return Ok($"Deleting shirt with id: {id}");
     }
   }
 }
